220372058d6ed6e6706f18d2f30b03a5b7481724
FatimaAlmashhor <fatima.amin4official@gmail.com>
2022-02-01 00:12:46 +0300

    Feat: profile page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // import 'bootstrap';
-// import 'bootstrap-icons/font/bootstrap-icons.css';
-import '../utils/jquery-3.6.0.min';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+// import '../utils/jquery-3.6.0.min';
 import './login'
 console.log('test');
