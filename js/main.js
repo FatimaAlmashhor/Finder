30c7f92751e4f26f9778913ed33701080e775a93
FatimaAlmashhor <fatima.amin4official@gmail.com>
2022-02-01 17:45:18 +0300

    Bug : main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
 // import 'bootstrap';
-import 'bootstrap-icons/font/bootstrap-icons.css';
 import '../utils/jquery-3.6.0.min';
 import './login'
 
 $(document).scroll(function () {
     // console.log('test');
     console.log(window.scrollY);
-    window.scrollY > 0 ? $('.navbar').css('background', 'black') : $('.navbar').css('background', 'transparent')
+    window.scrollY > 0 ? $('.navbar').css('background', 'hsl(237deg 17% 24% / 51%)') : $('.navbar').css('background', 'transparent')
 })
 // window.addEventListener('load', () => {
 //     if (window.scrollY > 0) {
