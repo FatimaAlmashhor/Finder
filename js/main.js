1901b27dd31c62d5e26ba5778d24d327a9acfdf8
FatimaAlmashhor <fatima.amin4official@gmail.com>
2022-02-01 17:40:11 +0300

    Test Jquery

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 // import 'bootstrap';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-// import '../utils/jquery-3.6.0.min';
+import '../utils/jquery-3.6.0.min';
 import './login'
-console.log('test');
+
+$(document).scroll(function () {
+    // console.log('test');
+    console.log(window.scrollY);
+    window.scrollY > 0 ? $('.navbar').css('background', 'black') : $('.navbar').css('background', 'transparent')
+})
+// window.addEventListener('load', () => {
+//     if (window.scrollY > 0) {
+//         //
+//     }
+// })
