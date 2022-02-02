919c545f46bcc85b7064ef2270f94d7045e63702
FatimaAlmashhor <fatima.amin4official@gmail.com>
2022-02-02 09:09:31 +0300

    Fix : search what and where

--- js/search.js
+++ js/search.js
@@ -1,13 +1,14 @@
+
 $('#what_search').on('keyup', () => {
     let word = $('#what_search').val().toLowerCase();
     $('.job_post .job_title').filter(function () {
-        $('.job_post ').toggle($(this).text().toLowerCase().indexOf(word) > -1);
+        $(this).parent().parent().parent().parent().toggle($(this).text().toLowerCase().indexOf(word) > -1);
     })
 })
 $('#where_search').on('keyup', function () {
-    let word = $(this).val().toLowerCase();
-
-    $('.job_post .').filter(function () {
-        $('.job_post').toggle($(this).text().toLowerCase().indexOf(word) > -1);
+    let word = $('#where_search').val().toLowerCase();
+    $('.job_location').filter(function () {
+        console.log($(this).text().toLowerCase().indexOf(word));
+        $(this).parent().parent().parent().parent().toggle($(this).text().toLowerCase().indexOf(word) > -1);
     })
 })
