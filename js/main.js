57e841816d0d31a5a1eb2d062ba36a3b86fd0fc8
FatimaAlmashhor <fatima.amin4official@gmail.com>
2022-02-02 11:29:17 +0300

    Feat : upload your cv feature

--- js/main.js
+++ js/main.js
@@ -5,3 +5,8 @@ $(document).scroll(function () {
     // console.log('test');
     window.scrollY > 0 ? $('.navbar').css('background', 'hsl(237deg 17% 24% / 51%)') : $('.navbar').css('background', 'transparent')
 })
+
+$('#download_cv').click(function (e) {
+    e.preventDefault();  //stop the browser from following
+    window.location.href = '../upload/FATIMA AMIN ALMASHHOR (1).pdf';
+});
