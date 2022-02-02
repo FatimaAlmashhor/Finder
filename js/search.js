9687b419566ef42f447711287f0ff82ce179094f
FatimaAlmashhor <fatima.amin4official@gmail.com>
2022-02-02 19:27:45 +0300

    Feat : checkbox filter

--- js/search.js
+++ js/search.js
@@ -19,3 +19,32 @@ $('#more_filter').click(function () {
 $('#all_btn').click(function () {
     $(this).toggleClass('bg-warning')
 })
+
+$(document).ready(() => {
+    const checkboxes = document.querySelectorAll("#job_type_filter input[type='checkbox']")
+    // const job_type = document.querySelectorAll(".job_type")
+    const job_post = document.querySelectorAll(".job_post")
+    checkboxes.forEach(element => {
+        element.addEventListener('change', (e) => {
+            if (e.target.checked) {
+                job_post.forEach(post => {
+                    let value = post.querySelector(`.job_type`).innerText;
+                    if (value.toLowerCase() === e.target.value.toLowerCase()) {
+                        post.classList.remove('d-none');
+                    }
+                    else {
+                        post.classList.add('d-none');
+                    }
+                })
+                // $(".job_post").hide();
+                // $(".job_post").attr(() => {
+                //     console.log($(this));
+                // });
+                // $(".job_type").filter(() => {
+                //     $(this).parent().parent().parent().parent().toggle(element.val === $(this).value)
+                // })
+            }
+
+        })
+    })
+})
